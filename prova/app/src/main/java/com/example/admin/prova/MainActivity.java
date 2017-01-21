7fd3565469ec1cfa6e42b56f45893cf3f29ce0b6
Arnau Infante <arnauinfantepinos@gmail.com>
2017-01-21 17:12:04 +0100

    Añade nuevo Activity para Sign Up, con diseño.

--- prova/app/src/main/java/com/example/admin/prova/MainActivity.java
+++ prova/app/src/main/java/com/example/admin/prova/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.admin.prova;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
 
         ImageView logoPagPrin = (ImageView) findViewById(R.id.LogoPagPrincipal);
@@ -22,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
         logoPagPrin.setImageResource(R.drawable.logo256);
         BtnLogIn.setImageResource(R.drawable.login);
         BtnSignIn.setImageResource(R.drawable.signup);
+
+        BtnLogIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        BtnSignIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent1 = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent1);
+            }
+
+
+        });
     }
 }
